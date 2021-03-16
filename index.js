2daaa62ecc60b7b3c70f632695226c3c08e299a6
Allie <alliecaton3@gmail.com>
2021-03-16 14:48:55 -0400

    commit tree 981f3314ce0c1f2041c0a51f23c94d3491b7a256

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const t = time.split(':')
+  t.splice(2, 1)
+  const joined = t.join('')
+  const parsed = parseInt(joined, 10)
+  if (parsed < 1200) {
+    return 'Good Morning'
+  } else if (1200 <= parsed && parsed <= 1700) {
+    return 'Good Afternoon'
+  } else if (parsed > 1700) {
+    return 'Good Evening'
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = str
+}
